fb51af284e699549b542714c41062e8e6d0d1039
htdesign <h.webdesigns@gmail.com>
2021-10-07 12:58:21 +1300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  plugins: [
+    require('@tailwindcss/custom-forms'),
+  ],
+  theme: {
+    fontFamily: {
+      'sans'  : ['Roboto', 'sans-serif'],
+      'roboto': ['Roboto', 'sans-serif'],
+      'public': ['"Public Sans"', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        'rtr-green' : {
+            'dark'    : '#81A332',
+            'DEFAULT' : '#98C03C'
+        },
+        'rtr-teal'  : '#52A7A7',
+        'rtr-red'   : '#CF5345',
+        'rtr-dark'  : '#3A393E',
+      },
+      height: {
+        'masthead' : '480px'
+      },
+      borderRadius: {
+        '20' : '20%',
+        '40' : '40%',
+        '60' : '60%',
+        '80' : '80%',
+      }
+    }
+  }
+}
